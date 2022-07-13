50f558558920f33bb79708e1a9ffa239334e891e
Femi Oyeniyi <65442229+Dafirm@users.noreply.github.com>
2022-07-13 11:49:23 +0300

    updated

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,19 +24,19 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon' />
               <h5>Experience</h5>
-              <small>3+ Years Working</small>
+              <small>1+ Years Working</small>
             </article>
 
             <article className='about__card'>
               <FiUsers className='about__icon' />
-              <h5>Clients</h5>
-              <small>200+ Worldwide</small>
+              <h5>Network</h5>
+              <small>300+</small>
             </article>
 
             <article className='about__card'>
               <VscFolderLibrary className='about__icon' />
               <h5>Projects</h5>
-              <small>10+ Completed</small>
+              <small>8+ Completed</small>
             </article>
           </div>
           <p>
