1dc174a0831665ea97a2cb3c6cb0c7654a5ca8d6
Femi Oyeniyi <65442229+Dafirm@users.noreply.github.com>
2022-07-20 11:52:20 +0300

    updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,8 @@ const Header = ()  => {
       <div className='container header__container'>
         <h5>Hello I'm</h5>
         <h1>Femi Oyeniyi</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Fullstack Developer </h5>
+        <h6>(in view)</h6>
         <CTA />
         <div className='me'>
           <img src= {FEMI} alt ="Femi" />
